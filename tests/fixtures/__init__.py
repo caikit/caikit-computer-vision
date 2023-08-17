a76b39b117d4faf11e961205180bd6a53a116698
Alex-Brooks <Alex.Brooks@ibm.com>
2023-08-17 10:48:21 -0600

    Add bootstrap, load, save, run tests for yolos
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -11,15 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Standard
 import os
+
+# Third Party
 import pytest
 
+# Local
 from caikit_computer_vision.modules.object_detection import TransformersObjectDetector
 
 ### Constants used in fixtures
 FIXTURES_DIR = os.path.join(os.path.dirname(__file__))
 TINY_MODELS_DIR = os.path.join(FIXTURES_DIR, "tiny_models")
 TRANSFORMER_OBJ_DETECT_MODEL = os.path.join(TINY_MODELS_DIR, "YolosForObjectDetection")
+
 
 @pytest.fixture
 def detector_transformer_dummy_model():
