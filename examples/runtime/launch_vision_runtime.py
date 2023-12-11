d240f8f5e460668ce247e154295059ebe711c11e
Alex-Brooks <Alex.Brooks@ibm.com>
2023-12-11 07:31:54 -0700

    Add standalone launch script for runtime
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

---
+++ examples/runtime/launch_vision_runtime.py
@@ -0,0 +1,41 @@
+"""This demo is roughly derived from the example with the test fixture.
+"""
+# Standard
+import argparse
+from pathlib import Path
+import json
+import os
+import shutil
+import sys
+import tempfile
+
+# First Party
+import alog
+
+# Local
+from caikit.config.config import get_config
+from caikit.runtime.__main__ import main
+import caikit
+from dump_protos import export_protos
+from create_models_dir import init_models_dir
+from create_train_data import init_train_data
+
+def launch_runtime():
+    alog.configure(default_level="debug")
+    parser = argparse.ArgumentParser(
+        description="Launch the vision runtime"
+    )
+    parser.add_argument(
+        "--purge_dirs",
+        action="store_true",
+        help="Deletes and reinitializes existing models and protos"
+    )
+    args = parser.parse_args()
+    if args.purge_dirs:
+        init_models_dir()
+        init_train_data()
+        export_protos()
+    main()
+
+if __name__ == "__main__":
+    launch_runtime()
