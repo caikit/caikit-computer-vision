f5d8256c2537d3e0bf6e05afddba9a8eb405ea0b
Alex-Brooks <Alex.Brooks@ibm.com>
2023-12-11 07:53:50 -0700

    Add missing licenses
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- examples/runtime/common.py
+++ examples/runtime/common.py
@@ -1,3 +1,16 @@
+# Copyright The Caikit Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # Standard
 from pathlib import Path
 import os
@@ -9,9 +22,6 @@ import yaml
 # Add caikit computer vision to the syspath
 PROJECT_ROOT = os.path.join(Path(__file__).parent.parent.parent)
 sys.path.append(PROJECT_ROOT)
-# Standard
-# Make sure we can import without issue
-import os
 
 # Local
 import caikit_computer_vision
--- examples/runtime/run_train_and_inference.py
+++ examples/runtime/run_train_and_inference.py
@@ -1,5 +1,3 @@
-"""Runs a sample train [currently a stub], and exports the trained model under a new ID.
-"""
 # Copyright The Caikit Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Runs a sample train [currently a stub], and exports the trained model under a new ID.
+"""
 
 # Standard
 from pathlib import Path
